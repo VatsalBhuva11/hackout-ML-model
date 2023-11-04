f334b6ca61e717178cb509e6ef4ec8b3edb2d45b
VatsalBhuva11 <vatsalbhuva11@gmail.com>
2023-11-05 00:55:47 +0530

    comment models

--- app.py
+++ app.py
@@ -22,9 +22,9 @@ import re
 from nltk.corpus import stopwords
 
 
-df = pd.read_csv('Symptom2Disease.csv')
+# df = pd.read_csv('Symptom2Disease.csv')
 
-df1 = pd.read_csv('Final dataset.csv')
+# df1 = pd.read_csv('Final dataset.csv')
 df1 = df1.drop(['Symptom_1','Symptom_2','Symptom_3','Symptom_4','Symptom_5','Symptom_6','Symptom_7','Symptom_8','Symptom_9','Symptom_10','Symptom_11','Symptom_12','Symptom_13','Symptom_14','Symptom_15','Symptom_16'],axis=1)
 df1.fillna('0',inplace = True)
 
